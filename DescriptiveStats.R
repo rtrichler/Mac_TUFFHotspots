dba25e2758a733dc9b5692fa5ae05d8891645c10
rtrichler <rbtrichler@Rachels-MacBook-Pro.local>
2016-01-12 21:56:52 -0500

    more descriptive stats

--- DescriptiveStats.R
+++ DescriptiveStats.R
@@ -40,9 +40,49 @@ barplot(proj_bySector, main = "Number of Projects by Sector",
 title(ylab="Number of Projects", line=2, cex.lab=1.2, family="Calibri Light")
 
 #count of locations by sector
-loc_bySector=table(lev1a$ad_sector_codes)
+loc_bySector=table(lev1a$ad_sector_names)
 View(loc_bySector)
 
 barplot(loc_bySector)
 
+#subset by precision code
+loc_prec <- subset(loc, precision_code==1 | precision_code==2)
+table(loc_prec$precision_code)
 
+lev1a_prec <- subset(lev1a, precision_code==1 | precision_code==2)
+
+#get # of locations per project
+#unique_proj_prec = table(loc_prec$project_id)
+#View(unique_proj_prec)
+sort(table(loc_prec$project_id))
+
+# count of precision 1 or 2 locations by sector
+loc_bySector=table(lev1a_prec$ad_sector_codes)
+View(loc_bySector)
+
+barplot(loc_bySector)
+
+loc_bySectorRec=table(lev1a_prec$ad_sector_codes, lev1a_prec$recipients)
+barplot(loc_bySectorRec, legend=rownames(loc_bySectorRec))
+
+#locations by sector
+margin.table(loc_bySectorRec,1)
+
+#locations by country
+margin.table(loc_bySectorRec,2)
+
+#count precise projects with more than x locations
+
+unique_proj_prec = table(loc_prec$project_id)
+View(unique_proj_prec)
+
+sum(unique_proj_prec == 1)
+sum(unique_proj_prec > 1)
+sum(unique_proj_prec > 3)
+sum(unique_proj_prec > 5)
+
+
+install.packages("gmodels")
+library(gmodels)
+
+CrossTable(lev1a_prec$ad_sector_codes, lev1a_prec$project_id)
